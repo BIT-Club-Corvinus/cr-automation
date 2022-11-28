f497304e5dcb83ce7e613d43e92f6847335fa75c
corvinus-ijklsh <emese.vastag@stud.uni-corvinus.hu>
2022-11-28 21:55:57 +0100

    Update status.py

--- status.py
+++ status.py
@@ -7,12 +7,15 @@ Created on Fri Nov 18 15:47:01 2022
 
 import win32com.client as client
 import xlsxwriter
-import datetime
+from datetime import datetime
+from datetime import date
 import pandas as pd
 import numpy as np
 
 outlook = client.Dispatch("Outlook.Application")
 mapi = outlook.GetNamespace("MAPI")
+df = pd.DataFrame(columns = ['partner','subject', 'date','time'])
+current_date = datetime.now()
 
 # read an email
 def getSenderAddress(msg):
@@ -24,11 +27,11 @@ def getSenderAddress(msg):
 
 # folder codes - https://learn.microsoft.com/en-us/office/vba/api/outlook.oldefaultfolders
 
-df = pd.DataFrame(columns = ['partner', 'subject', 'content', 'date', 'time'])
 
-#Folder inbox
-inbox = mapi.GetDefaultFolder(6)
-inbox_messages = inbox.Items
+#Get folder inbox
+salesMessagesIn = mapi.Folders("bit-bce-salesteam@bce.bitclub.hu").Folders(2)
+
+inbox_messages = salesMessagesIn.Items
 inbox_messages.Sort('ReceivedTime')
 inbox_length = len(inbox_messages)
 
@@ -38,37 +41,40 @@ for i in range(inbox_length):
     except:
         sender = None
     subject = inbox_messages[i].Subject
-    content = inbox_messages[i].Body
     try:
         date = inbox_messages[i].SentOn.strftime("%Y.%m.%d")
         time = inbox_messages[i].SentOn.strftime("%H:%M:%S")
     except:
         date = ""
         time = ""
-    email = {'partner':sender, 'subject':subject, 'content':content, 'date':date, 'time':time}
-    df = df.append(email, ignore_index = True)
+    email = {'partner':sender, 'subject':subject, 'date':date, 'time':time}
+    df_email_temp = pd.DataFrame([email])
+    df = pd.concat([df,df_email_temp], ignore_index = True)
+    
+
+
     
 #Sent email inbox
-sent = mapi.GetDefaultFolder(5)
-sent_messages = sent.Items
-sent_messages.Sort('ReceivedTime')
-sent_length = len(sent_messages)
+# sent = mapi.GetDefaultFolder(5)
+# sent_messages = sent.Items
+# sent_messages.Sort('ReceivedTime')
+# sent_length = len(sent_messages)
 
-for i in range(sent_length):
-    recip = ""
-    for recipents in sent_messages[i].Recipients:
-        recip = recip + ' ' + recipents.Address
-    subject = sent_messages[i].Subject
-    content = sent_messages[i].Body
-    email = {'partner':recip, 'subject':subject, 'content':content}
-    try:
-        date = sent_messages[i].SentOn.strftime("%Y.%m.%d")
-        time = sent_messages[i].SentOn.strftime("%H:%M:%S")
-    except:
-        date = ""
-        time = ""
-    email = {'partner':sender, 'subject':subject, 'content':content, 'date':date, 'time':time}
-    df = df.append(email, ignore_index = True)
+# for i in range(sent_length):
+#     recip = ""
+#     for recipents in sent_messages[i].Recipients:
+#         recip = recip + ' ' + recipents.Address
+#     subject = sent_messages[i].Subject
+#     content = sent_messages[i].Body
+#     email = {'partner':recip, 'subject':subject, 'content':content}
+#     try:
+#         date = sent_messages[i].SentOn.strftime("%Y.%m.%d")
+#         time = sent_messages[i].SentOn.strftime("%H:%M:%S")
+#     except:
+#         date = ""
+#         time = ""
+#     email = {'partner':sender, 'subject':subject, 'content':content, 'date':date, 'time':time}
+#     df = df.append(email, ignore_index = True)
 
 #Filter Teams invitations
 df.dropna(subset=['partner'], inplace = True)
@@ -76,12 +82,27 @@ df.dropna(subset=['partner'], inplace = True)
 
 
 #Filter other Teams and Akrivis messages
-df = df.loc[~df['partner'].str.contains("microsoft") & ~df['partner'].str.contains("akrivis")]
+df = df.loc[~df['partner'].str.contains("teams.microsoft") & ~df['partner'].str.contains("akrivis")]
 
-df.info()
+
 
 df_pivot = pd.pivot_table(df,index=['partner'], values = ['date','time'], aggfunc = np.max)
-df_pivot['subject_last_mail'] = df_pivot.merge(df,how='left',on='partner')['subject']
+df_pivot = df_pivot.reset_index()
+temp = pd.DataFrame()
+temp = pd.merge(df_pivot,df,how='left',on = 'partner')
+temp1 = pd.DataFrame()
+temp2 = pd.DataFrame()
+temp1['partner'] = df_pivot['partner'].copy()
+temp2['partner'] = temp['partner'].copy()
+temp2['subject'] = temp['subject'].copy()
+temp3 = pd.DataFrame()
+temp3 = pd.merge(temp1,temp2,how = 'left',on='partner')
+
+#df_pivot['subject_last_mail'] = pd.merge(df_pivot,temp,how='left',on='partner')['subject']
+df_pivot['Utolsó mail óta eltelt napok'] = (current_date - pd.to_datetime(df_pivot['date'])).dt.days
+
+df_pivot.info()
+df.info()
 
 #To do: a bit-sales email inboxára szűrve scraping
 #Kiírni Excelbe: distincten partnemailek, utolsó email subject, utolsó email dátum - idő, utolsó email óta eltelt napok száma, STÁTUSZ (lényeg)
