53aa341dfecc72c5d3c8bd2b03f6b32b5ff5f1d6
TuskeZsuzsanna <63471377+TuskeZsuzsanna@users.noreply.github.com>
2023-04-09 20:49:01 +0200

    gmailes címeket is kezeli bites kapcsolattartót megjeleníti

--- data_to_excel_guide.py
+++ data_to_excel_guide.py
@@ -14,8 +14,6 @@ salesMessagesIn = mapi.Folders("bit-bce-salesteam@bce.bitclub.hu").Folders(2).It
 salesMessagesIn.Sort('ReceivedTime', True)
 
 
-
-
 #Mai dátum "létrehozása" az excel nevéhez
 nowdate = datetime.datetime.now()
 nowdate = nowdate.strftime("%d_%m_%Y_%Hh%Mm%Ss")
@@ -33,6 +31,7 @@ worksheet.write(1, 4, 'Time')
 worksheet.write(1, 5, 'Subject')
 worksheet.write(1, 6, 'Days since last mail')
 worksheet.write(1, 7, 'State')
+worksheet.write(1,8, 'Contact')
 
 #lc = line Cunter az excel fájl sorain léptetéshez
 lc = 2
@@ -51,29 +50,16 @@ for m in salesMessagesIn:
     #pName = Partnercég neve
     #sMail = Sender Mail a küldő emailcíme
     #allpMail = Minden NEM bites, YBG, vagy ubc cím ide kerül, ezt írjuk a CCs oszlopba
+    #bitMail = bites kapcsolattartó neve
+    #bitMails = ide gyűjti a bites címzetteket
     allpMail=""
     pMail=""
     pName=""
     sMail=""
+    bitMail=""
+    bitMails=[]
     
-    #Végig megyünk az aktuális üzenet CC-in
-    for r in Recip:
-        if (r.AddressEntry.Type == "EX"):
-            pMail = str(r.AddressEntry.GetExchangeUser().PrimarySmtpAddress)
-            
-            #Ha a CC nem bites mailcím, akkor hozzá adjuk ahhoz a változóhoz amit beírunk majd a "CCs" oszlopba illetve a "Partner" oszlopba
-            if ("bce.bitclub.hu" not in pMail):
-                allpMail+=(pMail+", ")
-                pName = pMail.split('@')[1]
-                
-        else:
-            pMail = str(r.AddressEntry.Address)
-            
-            #Ha a CC nem bites mailcím, akkor hozzá adjuk ahhoz a változóhoz amit beírunk majd a "CCs" oszlopba illetve a "Partner" oszlopba
-            if ("bce.bitclub.hu" not in pMail):
-                allpMail+=(pMail+", ")
-                pName = pMail.split('@')[1]
-                
+    #Sender vizsgálata
     try:
                  
         if m.SenderEmailType == "EX":
@@ -86,12 +72,50 @@ for m in salesMessagesIn:
         if ("bce.bitclub.hu" not in sMail):
             allpMail+=(sMail+", ")
             pName = sMail.split('@')[1]
+            if (pName  == "gmail.com"):
+                pName = sMail.split('@')[0]
+        #Ha a Sender bites mailcím, akkor hozzá adjuk ahhoz a változóhoz amit beírunk majd a "Contact" oszlopba
+        else:
+            bitMail=sMail.split('@')[0]
         
     except:
         print("Nem rendes mail.")
-    
+
+    #Végigmegyünk az aktuális üzenet CC-in
+    for r in Recip:
+        
+        if (r.AddressEntry.Type == "EX"):
+            pMail = str(r.AddressEntry.GetExchangeUser().PrimarySmtpAddress) 
+        
+        else:
+            pMail = str(r.AddressEntry.Address)
+            
+        #Ha a CC nem bites mailcím, akkor hozzáadjuk ahhoz a változóhoz amit beírunk majd a "CCs" oszlopba illetve a "Partner" oszlopba
+        if ("bce.bitclub.hu" not in pMail):
+            allpMail+=(pMail+", ")
+            pName = pMail.split('@')[1]
+            if (pName  == "gmail.com"):
+                pName = pMail.split('@')[0]
+        
+        #Ha a CC bites és nem bites küldte, akkor a címzetteket összegyűjti a bitMails változóba
+        elif("bce.bitclub.hu" not in sMail):
+            bitMails.append(pMail.split('@')[0].lower())
+
+        #kiválasztja a bites címzettek közül(bitMails elemei) a kapcsolattartót(bitMail)
+        if "bce-bitclub-salesteam" in bitMails:
+                bitMails.delete(bitMails.index("bce-bitclub-salesteam"))
+        for i in bitMails:
+            if not(i=="lili.torok") and not(i=="gergo.komezei"):
+                bitMail=i
+                break
+        if bitMail=="":
+            for i in bitMails:
+                bitMail+=(i+', ')
+        
+  
     worksheet.write(lc, 1, pName)        
     worksheet.write(lc, 2, allpMail)
+    worksheet.write(lc, 8, bitMail)
     
     #Dátum és idő kinyerése
     mDateTime = str(m.ReceivedTime)
@@ -118,6 +142,7 @@ for m in salesMessagesIn:
         state = 1
     else:
         state= 0
+
         
     worksheet.write(lc, 7, state)   
     
@@ -125,6 +150,3 @@ for m in salesMessagesIn:
     
     
 workbook.close()
-    
-
-alma = "alma"
